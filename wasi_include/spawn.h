b65564f988498c8a563cff85bfc3aeb16a18e3e8
yusangeng <yusangeng@outlook.com>
2026-04-04 13:27:02 +0800

    refactor(wasi): remove unused header files
    Remove several header files that are no longer needed in the WASI
    build environment:
    
    - Remove linux/fs.h containing filesystem related constants and ioctls
    - Remove setjmp.h with custom implementation for WASM builds
    - Remove spawn.h with posix_spawn stub implementations
    - Remove sys/mount.h with mount system call stubs
    - Remove sys/shm.h with shared memory related definitions
    - Remove sys/sysinfo.h with system information structure and stub
    
    These headers were providing compatibility layers for WASI builds but
    are now being removed as they are either obsolete or replaced by other
    mechanisms.

--- wasi_include/spawn.h
+++
@@ -1,5 +0,0 @@
-#ifndef _SPAWN_H
-#define _SPAWN_H
-static inline int posix_spawn(pid_t *r, const char *p, void *fa, void *sa, char *const av[], char *const ev[]) { return -1; }
-static inline int posix_spawnp(pid_t *r, const char *p, void *fa, void *sa, char *const av[], char *const ev[]) { return -1; }
-#endif
